4783c60b5f66556f7dd0a8bbf7e3eb4ca698ab7f
Alon Dai <alondai1@gmail.com>
2021-12-13 11:07:32 +0200

    added show toggle

--- client/src/SelectedQuotes/SelectedQuotes.jsx
+++ client/src/SelectedQuotes/SelectedQuotes.jsx
@@ -1,41 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './SelectedQuotes.scss'
 import ExitIcon from './exit-icon.png'
 
+
 const mockData = [
     { txt: '"לי היה כדאי להישאר בחיים כשאר נולדו הבנים אמרתי לעצמי שאולי בכל זאת היה כדאי להישאר בחיים.\n הקמתי משפחה לתפארת - רק לא לי אלא לעם ישראל"' },
-     {txt: '"באושוויץ כתבתי על פיסות נייר של שקי מלט. אך את אותן רשימות לא הצלחתי לשמור עד יום השחרור. \n בעצם - לא רשמתי אותן במחשבה לנצלן באחד הימים - אלא פשוט, באופן כלשהו בן עזרו לי להמשיך לחיות"'} ,
+    { txt: '"באושוויץ כתבתי על פיסות נייר של שקי מלט. אך את אותן רשימות לא הצלחתי לשמור עד יום השחרור. \n בעצם - לא רשמתי אותן במחשבה לנצלן באחד הימים - אלא פשוט, באופן כלשהו בן עזרו לי להמשיך לחיות"' },
     {}
 ]
 
-export default function SelectedQuotes({ quotes }) {
+export default function SelectedQuotes({ quotes, isShown }) {
 
-    const onExit = (e) =>{
-        console.log('exit');
+    const [testIsShown, setTestIsShown] = useState(true)
+
+    const onExit = (e) => {
+        setTestIsShown(false)
     }
 
-    const handleClick = (e) =>{
+    const handleClick = (e) => {
         console.log(e);
     }
 
     return (
-        <div className='selected-quotes'>
-            <div onClick={onExit} className='icon-container'>
-                <img src={ExitIcon} className='pointer' />
-            </div>
-            <section className='content'>
-                <h1>ציטוטים נבחרים</h1>
-                <div className='quotes-container'>
-                    {
-                        mockData.map(currQuote => {
-                            return <Quote>{currQuote.txt}</Quote>
-                        })
-                    }
+        <>
+            {
+                testIsShown && <div className='selected-quotes'>
+                    <div onClick={onExit} className='icon-container'>
+                        <img src={ExitIcon} className='pointer' />
+                    </div>
+                    <section className='content'>
+                        <h1>ציטוטים נבחרים</h1>
+                        <div className='quotes-container'>
+                            {
+                                mockData.map((currQuote, idx) => {
+                                    return <Quote key={idx}> {currQuote.txt}</Quote>
+                                })
+                            }
+                        </div>
+                        <div className='or-section'><span>או</span></div>
+                        <button onClick={handleClick} className='pointer'>סימון חופשי</button>
+                    </section>
                 </div>
-                <div className='or-section'><span>או</span></div>
-                <button onClick={handleClick} className='pointer'>סימון חופשי</button>
-            </section>
-        </div>
+            }
+        </>
     )
 }
 
